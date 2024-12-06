929da0014c94ff05fc133c3575ab0b6a60aa7ac9
curiousjaki <39407479+curiousjaki@users.noreply.github.com>
2024-12-06 13:06:34 +0100

    bug: issues with serialization

--- projects/rules_new/methods/guest/src/bin/verifiable_processing.rs
+++ projects/rules_new/methods/guest/src/bin/verifiable_processing.rs
@@ -1,9 +1,15 @@
 //#![no_main]
 use risc0_zkvm::guest::env;
 use operations::Operation;
+use qfilter::Filter;
+use std::string::String;
+use rules::{RuleSet};
+
 fn main() {
     // read the input
-    let (a,b,operation): (f64,f64,Operation) = env::read();
+    let (a,b,operation, rule_set): (f64,f64,Operation, RuleSet) = env::read();
+
+
     let result: f64 = match operation {
         Operation::Add => a + b,
         Operation::Sub => a - b,
@@ -14,7 +20,12 @@ fn main() {
     // write public output to the journal
     //env::commit(&process_id);
     //env::commit(&SystemTime::now());
-    env::commit(&result);
+
+    let json: String = serde_json::to_string(&rule_set.qf).unwrap();
+    //env::write(&json.as_str());
+
+    env::commit(&(&result,&json));
+    //env::commit(&json.as_bytes()); 
 
     //env::commit(&emission_factor);
 }
---
+++ projects/rules_new/rules/src/lib.rs
@@ -0,0 +1,171 @@
+fn char_to_u32_array(ch: char) -> [u32; 8] {
+    let mut result = [0u32; 8];
+    let code_point = ch as u32; // Convert the char to its Unicode code point
+    
+    for i in 0..8 {
+        // Extract each bit from the code point and store it in the array
+        result[7 - i] = (code_point >> i) & 1;
+    }
+    
+    result
+}
+
+trait InsertEvent {
+    fn insert_event(&mut self, event: [u32;8], rules: Vec<OrderingRule>) -> Result<(), qfilter::Error>;
+    
+}
+
+impl InsertEvent for qfilter::Filter {
+
+    /// Inserts an event into the filter and adds a FollowedBy relation if an ordering rule is fulfilled
+    fn insert_event(&mut self, event: [u32;8], rules: Vec<OrderingRule>) -> Result<(), qfilter::Error>{
+        for rule in rules.iter() {
+            if rule.next == event {
+                if self.contains(rule.prior) {
+                    self.insert_duplicated(FollowedBy { prior: rule.prior, next: rule.next }).unwrap();
+                }
+            }
+        }
+        return self.insert_duplicated(event);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut qf: qfilter::Filter = qfilter::Filter::new(100, 0.01).unwrap();
+        let events : [char;5] = ['a','a','b','c','d'];
+        let current_event = char_to_u32_array('d');
+        for event in events.iter(){
+            qf.insert_event(char_to_u32_array(*event),vec![OrderingRule{prior:char_to_u32_array('a'),next:char_to_u32_array('c')}]).unwrap(); 
+        }
+        
+        let rules: Vec<Rule> = vec![
+            Rule::Precedence(PrecedenceRule{most_recent: current_event, preceeding: char_to_u32_array('d')}),
+            //Rule::Precedence(PrecedenceRule{current: current_event, prior: char_to_u32_array('d')}),
+            Rule::Cardinality(CardinalityRule{prior: char_to_u32_array('b'),max: 1, min: 1}),
+            Rule::Cardinality(CardinalityRule{prior: char_to_u32_array('a'),max: 2, min: 2}),
+            Rule::Exclusiveness(ExclusivenessRule{prior_a:  char_to_u32_array('a'), prior_b:  char_to_u32_array('e')}),
+            Rule::Ordering(OrderingRule{prior: char_to_u32_array('a'),next: char_to_u32_array('c')}),
+        ];
+       //let filter_string = serde_json::to_string(&qf).unwrap();
+        let task_rules = RuleSet{rules: rules, qf};
+        let res = serde_json::to_string(&task_rules).unwrap();
+        //write a string to a file
+        std::fs::write("rules.json", &res).unwrap();
+        println!("{:?}",&res);
+
+        for rule in task_rules.rules.iter(){
+            assert_eq!(rule.check(&task_rules.qf),true);
+        }
+    }
+}
+
+
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct RuleSet {
+    pub rules: Vec<Rule>,
+    pub qf: qfilter::Filter,
+} 
+
+pub trait RuleChecker {
+    fn check(&self, qf: &qfilter::Filter) -> bool;
+}
+
+impl RuleChecker for Rule {
+    fn check(&self, qf: &qfilter::Filter) -> bool {
+        match self {
+            Rule::Precedence(rule) => rule.check(qf),
+            Rule::Cardinality(rule) => rule.check(qf),
+            Rule::Exclusiveness(rule) => rule.check(qf),
+            Rule::Ordering(rule) => rule.check(qf),
+        }
+    }
+}
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+struct FollowedBy {
+    prior: [u32; 8],
+    next: [u32; 8],
+}
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+pub struct PrecedenceRule {
+    preceeding: [u32; 8],
+    most_recent: [u32; 8],
+    //prior_image_id: [u32; 8],
+}
+impl RuleChecker for PrecedenceRule {
+    // meaning that the prior event happend anytime before this event. not necessarily directly before
+    // to do precedence one would have to evaluate the previous events signature.
+    fn check(&self, qf: &qfilter::Filter) -> bool {
+        if qf.contains(&self.most_recent) && self.preceeding == self.most_recent {
+            return true
+        }
+        false
+    }
+}
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+pub struct CardinalityRule {
+    prior: [u32; 8],
+    min: u64,
+    max: u64,
+}
+
+impl RuleChecker for CardinalityRule {
+    fn check(&self, qf: &qfilter::Filter) -> bool {
+        let mut mut_qf = qf.clone();
+        if mut_qf.count(self.prior) >= self.min && mut_qf.count(self.prior) <= self.max {
+            return true
+        }
+        false
+    }
+}
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+pub struct ExclusivenessRule {
+    prior_a: [u32; 8],
+    prior_b: [u32; 8],
+}
+
+impl RuleChecker for ExclusivenessRule {
+    fn check(&self, qf: &qfilter::Filter) -> bool {
+        if qf.contains(&self.prior_a) || qf.contains(&self.prior_b) {
+            if qf.contains(&self.prior_a) && qf.contains(&self.prior_b) {
+                return false
+            }
+            return true
+        }
+        false
+    }
+}
+
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+pub struct OrderingRule {
+    prior: [u32; 8],
+    next: [u32; 8],
+}
+
+impl RuleChecker for OrderingRule {
+    fn check(&self, qf: &qfilter::Filter) -> bool {
+        if qf.contains(&FollowedBy { prior: self.prior, next: self.next }) {
+            return true
+        }
+        false
+    }
+}
+
+#[derive(Debug, Hash, serde::Serialize, serde::Deserialize)]
+pub enum Rule {
+    Precedence(PrecedenceRule),
+    Cardinality(CardinalityRule),
+    Exclusiveness(ExclusivenessRule),
+    Ordering(OrderingRule),
+}
